e60b40da2315c0018ef6798785cd972097521f0c
KJW <57553647+nogosan@users.noreply.github.com>
2021-11-27 13:26:17 +0900

    work

--- lib/firebase_auth/current_auth_widget.dart
+++ lib/firebase_auth/current_auth_widget.dart
@@ -3,12 +3,6 @@ import 'package:firebase_reference/widget/loading_view.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-class AuthStatus {
-  bool signedIn;
-  String uid;
-  AuthStatus(this.signedIn, {this.uid = 'none'});
-}
-
 class CurrentAuthWidget extends StatefulWidget {
   const CurrentAuthWidget({Key? key}) : super(key: key);
 
@@ -17,31 +11,46 @@ class CurrentAuthWidget extends StatefulWidget {
 }
 
 class _CurrentAuthWidgetState extends State<CurrentAuthWidget> {
-  Future<AuthStatus> tryLoad() async {
-    bool signedIn = await FirebaseAuth.instance.currentUser != null;
-    if (!signedIn) return AuthStatus(signedIn);
-    else return AuthStatus(signedIn, uid: FirebaseAuth.instance.currentUser!.uid);
+  String uid = 'loading';
+
+  Future<void> load() async {
+    setState(() {
+      uid = '로딩 중...';
+    });
+
+    if (FirebaseAuth.instance.currentUser == null) {
+      setState(() {
+        uid = 'Auth 인증 되어있지 않음';
+      });
+      return;
+    }
+
+    User user = FirebaseAuth.instance.currentUser!;
+    await user.reload();
+
+    setState(() {
+      uid = 'uid: ${user.uid}';
+    });
+    return;
   }
 
   @override
   void initState() {
     super.initState();
+    load();
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: tryLoad(),
-      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-        if (snapshot.hasData) {
-          return Text('uid: ${(snapshot.data as AuthStatus).uid}');
-        } else if (snapshot.hasError) {
-          Fluttertoast.showToast(msg: snapshot.error.toString());
-          return const Text('로드에 실패했습니다.');
-        } else {
-          return LoadingView();
-        }
-      },
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(uid),
+        IconButton(
+          onPressed: () => load(),
+          icon: Icon(Icons.refresh, color: Colors.black),
+        )
+      ],
     );
   }
 }
